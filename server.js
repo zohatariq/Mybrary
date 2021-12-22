1fcae8629570a9e6cee964bf073af5b4a9bf0174
zohatariq <zohatariq2013@gmail.com>
2021-12-22 20:37:31 +0500

    File Upload Database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV !== 'production'){
-   require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 
 const express = require('express')
@@ -12,22 +12,20 @@ const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
-app.set('views',__dirname + '/views')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {
-useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-
 app.use('/', indexRouter)
-app.use('/authors',authorRouter)
-app.use('/books',bookRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
